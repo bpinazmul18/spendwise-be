d3c577afae3dd4e23389d51ee75798d1cbf448f9
Nazmul Haque <bpinazmul18@gmail.com>
2023-10-29 07:55:18 +0600

    init: project init with signin route

---
+++ startup/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+const config = require('config')
+const winston = require('winston')
+
+module.exports = function () {
+    const db = config.get('dbURI')
+    
+    mongoose.set("strictQuery", false);
+    mongoose.connect(db, { useNewUrlParser:true, useUnifiedTopology: true })
+        .then(() => winston.info(`Connected to MongoDB: ${db}`))
+}
